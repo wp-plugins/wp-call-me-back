1c43da26a10d97f778108701cbd0a7d715d61661
pigeonhut <pigeonhut@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-15 11:09:55 +0000

    committing version 1.18 callmeback
    git-svn-id: https://plugins.svn.wordpress.org/wp-call-me-back/trunk@1113014 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-callmeback.php
+++ wp-callmeback.php
@@ -5,7 +5,7 @@ ob_start();
  * Plugin Name: Call me back widget
  * Plugin URI: http://pigeonhut.com
  * Description: Request call me back widget by PigeonHUT
- * Version: 1.17
+ * Version: 1.19
  * Author: Jody Nesbitt (WebPlugins)
  * Author URI: http://webplugins.co.uk
  *
